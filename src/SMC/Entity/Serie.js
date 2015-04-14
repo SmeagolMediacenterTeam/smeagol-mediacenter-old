c52ca2ea9557b411e48065a37dc0d13178e194c4
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-04-14 17:55:53 +0200

    Add Entity

---
+++ src/SMC/Entity/Serie.js
@@ -0,0 +1,22 @@
+
+SMC.Entity.Series = new GollumJS.Class ({
+
+	/**
+	 * @ORM.Annotations.Column ({
+	 * 	name: "id",
+	 * 	type: "int",
+	 * });
+	 */
+	id: null,
+
+	/**
+	 * @ORM.Annotations.Column ({
+	 * 	name: "name",
+	 * 	type: "string",
+	 * });
+	 */
+	name: null,
+	
+	
+	
+});
